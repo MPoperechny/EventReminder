5e2a28a7a4bbb155aa165606bb91ecf9de1859ec
Maks <mpoperechny@gmail.com>
2019-11-16 12:56:37 +0300

    added NextEventActivity layout

--- app/src/main/java/ru/mpoperechny/eventreminder/NextEventActivity.kt
+++ app/src/main/java/ru/mpoperechny/eventreminder/NextEventActivity.kt
@@ -3,10 +3,10 @@ package ru.mpoperechny.eventreminder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NextEventActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_next_event)
     }
 }
