c01f13fa7d72031a8da7505b175917f8832baed4
Maksim Poperechny <mpoperechny@gmail.com>
2020-02-14 13:37:23 +0300

    added days left function

--- app/src/main/java/ru/mpoperechny/eventreminder/database/EventEntity.kt
+++ app/src/main/java/ru/mpoperechny/eventreminder/database/EventEntity.kt
@@ -1,17 +1,58 @@
 package ru.mpoperechny.eventreminder.database
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "events")
-class EventEntity(var date: Int, var type: Int, var person: String?, var description: String?) {
-
+data class EventEntity(
+    var date: Long,
+    var type: Int,
+    var person: String?,
+    var description: String?
+) {
 
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 
+    @delegate:Ignore
+    val daysLeft: Int by lazy { daysLeft(date) }
+
+    //@Ignore
+    //val mark = println("created EventEntity ${this.hashCode()}")
+
     companion object {
         var BIRTHDAY = 0
         var HOLIDAY = 1
+    }
+
+    private fun daysLeft(timeInMilliseconds: Long): Int {
+
+        //println("calculate daysLeft for ${this.hashCode()}")
+
+        val today = Calendar.getInstance()
+        val currentYear = today.get(Calendar.YEAR)
+        val todayDayOfYear = today.get(Calendar.DAY_OF_YEAR)
+
+        val targetDay = Calendar.getInstance().apply {
+            timeInMillis = timeInMilliseconds
+            set(Calendar.YEAR, currentYear)
+        }
+        val targetDayThisYear = targetDay.get(Calendar.DAY_OF_YEAR)
+
+        return when {
+            targetDayThisYear == todayDayOfYear -> 0
+            targetDay.after(today) -> targetDayThisYear - todayDayOfYear
+            else -> {
+                val targetDayNextYear = targetDay.apply { add(Calendar.YEAR, 1); }
+                    .get(Calendar.DAY_OF_YEAR)
+                val lastDayOfCurrentYear = Calendar.getInstance().apply { set(currentYear, 11, 31) }
+                    .get(Calendar.DAY_OF_YEAR)
+
+                lastDayOfCurrentYear - todayDayOfYear + targetDayNextYear
+            }
+        }
+
     }
 }
